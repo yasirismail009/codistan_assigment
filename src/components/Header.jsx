2bcc91710a970da9104cec9939852fd7ee1fbf4a
Muhammad Yasir Ismail <abdulrab916@gmail.com>
2023-09-07 11:24:35 +0500

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ export const Header = () => {
     <header loading='lazy' className={`bg-hero-mountains bg-cover h-screen bg-no-repeat saturate-[.8]  opacity-80`}>
       <Navbar bgColor="transparent" linkColor="white" />
 
-      <div className="flex flex-col justify-center items-center ">
-        {/* <h1 className="header-font  text-4xl sm:text-[7.1rem] lg:py-20 md:py-20 sm:py-8 py-8">LOS ANGELES</h1>
-        <span className="header-font text-4xl  sm:text-[7.1rem] blue-gradient tracking-wider ">
+      <div className="flex flex-col justify-center items-center h-full">
+        <h1 className="header-font lg:hidden md:hidden  text-4xl sm:text-[7.1rem] lg:py-20 md:py-20 sm:py-8 py-0 m-0">LOS ANGELES</h1>
+        <span className="header-font text-4xl lg:hidden md:hidden  sm:text-[7.1rem] blue-gradient tracking-wider m-0">
           MOUNTAINS
-        </span> */}
+        </span>
       </div>
     </header>
   );
